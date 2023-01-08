a0f530ec0a9d2014647c746f13ba7c7c05e2172e
DamjanT1988 <Damjan_t@hotmail.com>
2023-01-08 16:38:00 +0100

    1

--- Migyston-web-api/Models/Product.cs
+++ Migyston-web-api/Models/Product.cs
@@ -7,8 +7,18 @@ namespace Migyston_web_api.Models
 {
     public class Product
     {
+        //auto
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public bool IsGlutenFree { get; set; }
+        public DateTime Date { get; set; }
+
+        //user input
+        public string? Product_title { get; set; }
+        public string? Ean_number { get; set; }
+        public string? Product_description { get; set; }
+        public double? Price { get; set; }
+        public int? Amount_storage { get; set; }
+        public string? Expiration_date { get; set; }
+
+
     }
 }
--- Migyston-web-api/Services/ProductServices.cs
+++ Migyston-web-api/Services/ProductServices.cs
@@ -2,31 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using Migyston_web_api.Models;
+using System.Text.Json.Serialization;
+using System.Runtime.Intrinsics.X86;
+using System.Net.Http.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Migyston_web_api.Services
 {
     public static class ProductServices
     {
-        static List<Product> Products { get; }
-        static int nextId = 3;
+        static List<Product> Products { get; set; }
+        static int nextId = 1;
         static ProductServices()
         {
             Products = new List<Product>
-        {
-            new Product { Id = 1, Name = "Classic Italian", IsGlutenFree = false },
-            new Product { Id = 2, Name = "Veggie", IsGlutenFree = true }
-        };
+        {};
         }
 
-        public static List<Product> GetAll() => Products;
-
+        public static List<Product> GetAll()
+        {
+            LoadFile();
+            return Products;
+        }
         public static Product? Get(int id) => Products.FirstOrDefault(p => p.Id == id);
 
         public static void Add(Product product)
         {
             product.Id = nextId++;
+            product.Date = DateTime.Now;
             Products.Add(product);
+            SaveProducts();
         }
 
         public static void Delete(int id)
@@ -45,6 +52,41 @@ namespace Migyston_web_api.Services
                 return;
 
             Products[index] = product;
+        }
+        //save the list to a file
+        public static void SaveProducts()
+        {
+            //serialize the list to a variable
+            string json = JsonSerializer.Serialize(Products);
+            //write the JSON list to a file
+            File.WriteAllText(@Directory.GetCurrentDirectory().ToString() + "/productlist.json", json);
+        }
+
+        //method to load text from JSON, create the object/list 
+        static void LoadFile()
+        {
+            //read text from file, store in a variable as JSON-string
+            string jsonString = File.ReadAllText(Directory.GetCurrentDirectory().ToString() + "/productlist.json");
+
+            //deserialize the JSON-string
+            dynamic obj = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonString);
+
+            //loop through the objects, create new items in the list
+            
+            for (int i = 0; i < obj.Count; i++)
+            {
+                //add new item in list by Add-method
+                Products.Add(new Product { 
+                    Id = obj[i].Id, 
+                    Product_title = obj[i].Product_title, 
+                    Ean_number = obj[i].Ean_number,
+                    Product_description = obj[i].Product_description,
+                    Price = obj[i].Price,
+                    Amount_storage = obj[i].Amount_storage,
+                    Expiration_date = obj[i].Expiration_date,
+                    Date = obj[i].Date
+                });
+            }
         }
     }
 }
